ab34e1c92900cc780bcf563c3b7f8b0caf89669e
shiva <shiva.bernhard@shining-cat.fr>
2023-11-14 14:02:18 +0100

    dependencies update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
     const val appCompat = "1.6.1"
     const val datastore = "1.0.0"
-    const val material = "1.9.0"
+    const val material = "1.10.0"
     const val jupiter = "5.9.3"
     const val junit = "1.1.5"
     const val androidXcoreKtx = "1.10.1"
     const val hilt = "2.48.1"
-    const val hiltNavigation = "1.0.0"
+    const val hiltNavigation = "1.1.0"
     const val room_version = "2.6.0"
     const val composeMaterial3WindowSizeClass = "1.1.2"
     const val nav_version = "2.7.3"
@@ -23,5 +23,5 @@ object Versions {
     const val composeTV = "1.0.0-alpha09" // see https://developer.android.com/jetpack/androidx/releases/tv for releases announcements
     const val kotlinCompilerExtension = "1.5.4" // this has to be kept in sync with kotlin-gradle-plugin version. See https://developer.android.com/jetpack/androidx/releases/compose-kotlin for kotlin and compose compile version compatibility
     const val ktlintGradlePlugin = "11.6.1"
-    const val androidxLifeCycleProcess = "2.6.1"
+    const val androidxLifeCycleProcess = "2.6.2" // see https://developer.android.com/jetpack/androidx/releases/lifecycle for releases announcements
 }
