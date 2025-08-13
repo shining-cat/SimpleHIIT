dc72a028bfc42ad264c4383fd3aa669062851cd4
shiva <shiva.bernhard@shining-cat.fr>
2025-08-13 16:48:37 +0200

    lint fix

--- android/tv/ui/home/src/main/java/fr/shiningcat/simplehiit/android/tv/ui/home/HomeViewModel.kt
+++ android/tv/ui/home/src/main/java/fr/shiningcat/simplehiit/android/tv/ui/home/HomeViewModel.kt
@@ -35,7 +35,7 @@ class HomeViewModel
                     _screenViewState.emit(
                         homeViewStateMapper.map(it),
                     )
-            }
+                }
             }
         }
 
