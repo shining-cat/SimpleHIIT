adf072f0dc08fa3af719bafdca212e97eff3a28e
shiva <shiva.bernhard@shining-cat.fr>
2025-08-19 12:05:07 +0200

    updated all specific dialogs previews

--- android/mobile/ui/settings/src/main/java/fr/shiningcat/simplehiit/android/mobile/ui/settings/dialogs/SettingsEditNumberCyclesDialog.kt
+++ android/mobile/ui/settings/src/main/java/fr/shiningcat/simplehiit/android/mobile/ui/settings/dialogs/SettingsEditNumberCyclesDialog.kt
@@ -1,6 +1,5 @@
 package fr.shiningcat.simplehiit.android.mobile.ui.settings.dialogs
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,8 +7,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Devices.DESKTOP
+import androidx.compose.ui.tooling.preview.Devices.FOLDABLE
+import androidx.compose.ui.tooling.preview.Devices.PHONE
+import androidx.compose.ui.tooling.preview.Devices.TABLET
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import fr.shiningcat.simplehiit.android.mobile.ui.common.theme.SimpleHiitMobileTheme
 import fr.shiningcat.simplehiit.commonresources.R
@@ -52,18 +56,19 @@ private fun setNumberCyclesErrorMessage(error: Constants.InputError): Int =
     }
 
 // Previews
+// we add each device preview manually here instead of using PreviewScreenSizes
+// because we want no showSystemUi which overlaps the system status bar to the dialog preview
+@PreviewLightDark
+@PreviewFontScale
+@Preview(name = "Phone", device = PHONE, showSystemUi = false)
 @Preview(
-    showBackground = true,
-    showSystemUi = true,
-    device = Devices.PIXEL_4,
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "Phone - Landscape",
+    device = "spec:width=411dp,height=891dp,orientation=landscape,dpi=420",
+    showSystemUi = false,
 )
-@Preview(
-    showBackground = true,
-    showSystemUi = true,
-    device = Devices.PIXEL_4,
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
+@Preview(name = "Unfolded Foldable", device = FOLDABLE, showSystemUi = false)
+@Preview(name = "Tablet", device = TABLET, showSystemUi = false)
+@Preview(name = "Desktop", device = DESKTOP, showSystemUi = false)
 @Composable
 private fun SettingsEditNumberCyclesDialogPreview() {
     SimpleHiitMobileTheme {
