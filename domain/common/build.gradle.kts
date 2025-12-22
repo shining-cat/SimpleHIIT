6018a1549aa488ec8340cef9729f1c7ba6d54374
shiva <shiva.bernhard@shining-cat.fr>
2025-12-22 11:25:47 +0100

    testing verifications violations

--- domain/common/build.gradle.kts
+++ domain/common/build.gradle.kts
@@ -7,5 +7,7 @@ plugins {
 
 dependencies {
     implementation(projects.commonUtils)
+    // TEST: Adding forbidden dependency - domain modules cannot depend on data layer
+    implementation(projects.data)
     testImplementation(projects.testUtils)
 }
--- domain/common/src/main/java/fr/shiningcat/simplehiit/domain/common/DurationStringFormatter.kt
+++ domain/common/src/main/java/fr/shiningcat/simplehiit/domain/common/DurationStringFormatter.kt
@@ -1,5 +1,7 @@
 package fr.shiningcat.simplehiit.domain.common
 
+import java.util.Date
+
 data class DurationStringFormatter(
     val hoursMinutesSeconds: String,
     val hoursMinutesNoSeconds: String,
@@ -7,4 +9,10 @@ data class DurationStringFormatter(
     val minutesSeconds: String,
     val minutesNoSeconds: String,
     val seconds: String,
-)
+) {
+    companion object {
+        // TEST: Using deprecated Date() constructor to trigger deprecation warning
+        // NO @Suppress annotation so it gets caught by deprecation checker
+        fun getCurrentTimestamp(): Long = Date().time
+    }
+}
