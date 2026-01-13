fa9c85bf04b4843eddd66e020d9fc34ee40c3a45
shiva <shiva.bernhard@shining-cat.fr>
2026-01-13 16:26:50 +0100

    "Bump version to 0.05 for first GitHub release"

--- build-logic/convention/src/main/kotlin/fr/shiningcat/simplehiit/config/Config.kt
+++ build-logic/convention/src/main/kotlin/fr/shiningcat/simplehiit/config/Config.kt
@@ -24,8 +24,8 @@ object ConfigHandheld {
             targetSdkVersion = 36,
             compileSdkVersion = 36,
             applicationId = APPLICATION_ID,
-            versionCode = 23100004,
-            versionName = "0.04",
+            versionCode = 23100005,
+            versionName = "0.05",
             nameSpace = "fr.shiningcat.simplehiit.mobile.app",
         )
     val jvm =
@@ -42,8 +42,8 @@ object ConfigTv {
             targetSdkVersion = 36,
             compileSdkVersion = 36,
             applicationId = APPLICATION_ID,
-            versionCode = 23010004,
-            versionName = "0.04",
+            versionCode = 23010005,
+            versionName = "0.05",
             nameSpace = "fr.shiningcat.simplehiit.tv.app",
         )
     val jvm =
