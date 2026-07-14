7421f72e2cb1bf3bc58ca2ea88581e403c5d3499
shiva <shiva.bernhard@shining-cat.fr>
2026-07-14 11:18:32 +0200

    Bump version to 1.04

--- build-logic/convention/src/main/kotlin/fr/shiningcat/simplehiit/config/Config.kt
+++ build-logic/convention/src/main/kotlin/fr/shiningcat/simplehiit/config/Config.kt
@@ -28,8 +28,8 @@ object ConfigHandheld {
             targetSdkVersion = 36,
             compileSdkVersion = 37,
             applicationId = APPLICATION_ID,
-            versionCode = 23100103,
-            versionName = "1.03",
+            versionCode = 23100104,
+            versionName = "1.04",
             nameSpace = "fr.shiningcat.simplehiit.mobile.app",
         )
     val jvm =
@@ -46,8 +46,8 @@ object ConfigTv {
             targetSdkVersion = 36,
             compileSdkVersion = 37,
             applicationId = APPLICATION_ID,
-            versionCode = 23010103,
-            versionName = "1.03",
+            versionCode = 23010104,
+            versionName = "1.04",
             nameSpace = "fr.shiningcat.simplehiit.tv.app",
         )
     val jvm =
